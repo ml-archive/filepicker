bc019e80bf0a5196a9e688bb51a5113b22a9f7dc
Mario Sanoguera de Lorenzo <masl@nodesagency.com>
2016-11-15 15:31:45 +0000

    cleanup and added imageview to test example project

--- filepicker.example/src/main/java/dk/nodes/filepickerexample/FilePickerExampleActivity.java
+++ filepicker.example/src/main/java/dk/nodes/filepickerexample/FilePickerExampleActivity.java
@@ -1,11 +1,14 @@
 package dk.nodes.filepickerexample;
 
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import dk.nodes.filepicker.FilePickerActivity;
@@ -20,17 +23,18 @@ public class FilePickerExampleActivity extends AppCompatActivity {
     final String DEFAULT_FILE_IMAGES = "FILE IMAGES (startActivityForResult with intent.putExtra(FilePickerActivity.FILE, true);";
     final String DEFAULT_FILE_TYPE = "FILE TYPE (startActivityForResult with intent.putExtra(FilePickerActivity.FILE, true); AND intent.putExtra(FilePickerActivity.TYPE, FilePickerActivity.MIME_PDF); for example";
     final String DEFAULT_FILE_MULTIPLE_TYPES = "FILE MULTIPLE TYPES (startActivityForResult with intent.putExtra(FilePickerActivity.FILE, true); AND intent.putExtra(FilePickerActivity.MULTIPLE_TYPES, new String[]{FilePickerActivity.MIME_IMAGE, FilePickerActivity.MIME_PDF});";
-    ImageButton doneIb;
     Button typeBtn;
+    ImageView imageView;
+    Button goBtn;
     Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_picker_example);
-
+        imageView = (ImageView) findViewById(R.id.image_view);
         typeBtn = (Button) findViewById(R.id.type_btn);
-        doneIb = (ImageButton) findViewById(R.id.done_ib);
+        goBtn = (Button) findViewById(R.id.go_btn);
 
         typeBtn.setText(DEFAULT);
         newIntent();
@@ -69,7 +73,7 @@ public class FilePickerExampleActivity extends AppCompatActivity {
             }
         });
 
-        doneIb.setOnClickListener(new View.OnClickListener() {
+        goBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivityForResult(intent, MY_REQUEST_CODE);
@@ -83,6 +87,8 @@ public class FilePickerExampleActivity extends AppCompatActivity {
         if (requestCode == MY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 Toast.makeText(FilePickerExampleActivity.this, FilePickerUriHelper.getUriString(data), Toast.LENGTH_SHORT).show();
+                Drawable imageDrawable = new BitmapDrawable(getResources(), FilePickerUriHelper.getBitmap(FilePickerExampleActivity.this, data));
+                imageView.setBackground(imageDrawable);
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(FilePickerExampleActivity.this, "User Canceled", Toast.LENGTH_SHORT).show();
             } else if (resultCode == RESULT_FIRST_USER) {
